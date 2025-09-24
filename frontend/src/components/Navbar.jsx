a6bf8159ccb1ee01dd255956f3e03e19e87854d3
Himal Pache Magar <pachemagarhimal@gmail.com>
2025-09-24 14:45:22 +0100

    hi

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import "react-toastify/dist/ReactToastify.css";
 import LoginForm from "./LoginForm";
 import SignupForm from "./SignupForm";
 import { IoChatbubbleEllipsesOutline, IoSend } from 'react-icons/io5';
-import Chat from './Chat.jsx';
+import Chat from './chat.jsx';
 import axios from 'axios';
 import { backendUrl } from '../App';
 
