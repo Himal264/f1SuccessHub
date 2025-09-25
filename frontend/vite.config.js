e3f0e3bdb7d2fa99cc5dd76ef27392a76e869ff8
Himal Pache Magar <pachemagarhimal@gmail.com>
2025-09-25 18:02:27 +0100

    the new htts change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       '/api': {
-        target: 'http://localhost:9000', // Backend server URL
+        target: 'https://localhost:9000', // Backend server URL
         changeOrigin: true,
         secure: false,
       },
